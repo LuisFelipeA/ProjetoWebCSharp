8a1bfedce17410c0b24d8168e167ed53386d51ac
Luis Felipe <felipeadriani2@gmail.com>
2023-06-29 09:40:31 -0300

    Update seller and custom service exception

---
+++ SalesWebMVC/SalesWebMVC/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+
+namespace SalesWebMVC.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
